1c1a947244e82564ced4330a01d7abdef480e2bb
michaldoman <mjd.domanski@gmail.com>
2023-08-23 20:39:39 +0200

    registering urls

--- Renovation/Renovation/urls.py
+++ Renovation/Renovation/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from main_app.views import HomeListView, AddItemView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include("django.contrib.auth.urls")),
     path('', HomeListView.as_view(), name='home'),
     path('add-item/', AddItemView.as_view(), name="add_item"),
 ]
