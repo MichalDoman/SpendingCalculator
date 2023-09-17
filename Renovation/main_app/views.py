03d0ad465ca37fccb43faf6d360ab1d742ad2b1b
michaldoman <mjd.domanski@gmail.com>
2023-09-17 20:11:32 +0200

    register view

--- Renovation/main_app/views.py
+++ Renovation/main_app/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView, CreateView, FormView
 from django.urls import reverse_lazy
+from django.contrib.auth import login
+from django.contrib.auth.models import User
 
 from main_app.forms import HomeListFilterForm, RegisterForm
 from main_app.models import Purchase
@@ -83,4 +85,26 @@ class AddItemView(CreateView):
 class RegisterView(FormView):
     """A View that enables users to register via a form, and access all functionalities."""
     form_class = RegisterForm
+    template_name = "registration/register.html"
 
+    def form_valid(self, form):
+        email = form.cleaned_data['email']
+        password = form.cleaned_data['password']
+        password_repeated = form.cleaned_data['password_repeated']
+
+        if User.objects.filter(email=email).exists():
+            form.add_error(None, 'This e-mail address is already taken!')
+            return super().form_invalid(form)
+
+        if password != password_repeated:
+            form.add_error(None, 'Passwords did not match!')
+            return super().form_invalid(form)
+
+        user = User.objects.create_user(
+            username=email,
+            email=email,
+            password=password,
+        )
+
+        login(self.request, user)
+        return super().form_valid(form)
